b24730c94c19d29926967ba2ab03394e6f891381
1151680016@qq.com <1151680016@qq.com>
2019-04-09 21:16:10 +0800

    first commit

---
+++ Js/tea_info.js
@@ -0,0 +1,17 @@
+
+$("#btn_down").click(function(){
+   	$("#step1").css("display","none");
+   	$("#step2").css("display","inline");
+	
+	//$("#form_step1").css("display","none");
+	//$("#foem_step2").css("display","inline");
+});
+$("#btn_up").click(function(){
+	$("#step1").css("display","inline");
+	$("#step2").css("display","none");
+	
+	
+	//$("#form_step2").css("display","none");
+	//$("#form_step1").css("display","inline");
+});
+		
