2da49d0bfcae9330b967d9a2c115a8821eb95d4f
1151680016@qq.com <1151680016@qq.com>
2019-04-10 19:25:00 +0800

    教师信息审核页面完成

--- Js/tea_info.js
+++ Js/tea_info.js
@@ -14,4 +14,20 @@ $("#btn_up").click(function(){
 	//$("#form_step2").css("display","none");
 	//$("#form_step1").css("display","inline");
 });
+
+$("#m_btn_down").click(function(){
+   	$("#Modal_step1").css("display","none");
+   	$("#Modal_step2").css("display","inline");
+	
+	//$("#form_step1").css("display","none");
+	//$("#foem_step2").css("display","inline");
+});
+$("#m_btn_up").click(function(){
+	$("#Modal_step1").css("display","inline");
+	$("#Modal_step2").css("display","none");
+	
+	
+	//$("#form_step2").css("display","none");
+	//$("#form_step1").css("display","inline");
+});
 		
